1940b1e83e66fb009f12f4a9feec92d4e837604e
root <root@cox.nokia.celabs>
2024-01-11 10:02:00 -0500

    infra ipv4/ipv6

---
+++ sros-config-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+yes | cp ./clab-cox/ixr-r1/tftpboot/config.txt ./config/ixr-r1.cfg
+yes | cp ./clab-cox/ixr-r2/tftpboot/config.txt ./config/ixr-r2.cfg
+yes | cp ./clab-cox/ixr-r3/tftpboot/config.txt ./config/ixr-r3.cfg
+yes | cp ./clab-cox/ixr-r4/tftpboot/config.txt ./config/ixr-r4.cfg
+yes | cp ./clab-cox/sr-r5/tftpboot/config.txt ./config/sr-r5.cfg
+yes | cp ./clab-cox/sr-r6/tftpboot/config.txt ./config/sr-r6.cfg
+yes | cp ./clab-cox/sr-r7/tftpboot/config.txt ./config/sr-r7.cfg
+yes | cp ./clab-cox/sr-r8/tftpboot/config.txt ./config/sr-r8.cfg
+yes | cp ./clab-cox/sr-r9/tftpboot/config.txt ./config/sr-r9.cfg
+yes | cp ./clab-cox/sr-r10/tftpboot/config.txt ./config/sr-r10.cfg
+yes | cp ./clab-cox/sr-r11/tftpboot/config.txt ./config/sr-r11.cfg
+yes | cp ./clab-cox/sr-r12/tftpboot/config.txt ./config/sr-r12.cfg
+yes | cp ./clab-cox/ixr-r13/tftpboot/config.txt ./config/ixr-r13.cfg
+yes | cp ./clab-cox/ixr-r14/tftpboot/config.txt ./config/ixr-r14.cfg
+yes | cp ./clab-cox/ixr-r15/tftpboot/config.txt ./config/ixr-r15.cfg
+yes | cp ./clab-cox/ixr-r16/tftpboot/config.txt ./config/ixr-r16.cfg
